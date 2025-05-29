276962d7478cea25da57ab34c3ba2fd4f6c5d1b5
jorge1sbr <jorge.p1998@hotmail.com>
2025-05-29 10:45:22 +0200

    Reseteo de DatosJuego a static

--- dragonBall/src/modelo/DatosJuego.java
+++ dragonBall/src/modelo/DatosJuego.java
@@ -2,60 +2,27 @@ package modelo;
 
 import daoDragonBall.*;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
+
 
 public class DatosJuego {
 
-    private ArrayList<Protagonista> protagonistas;
-    private ArrayList<PersonajeCombatiente> enemigos;
-    private ArrayList<PersonajeSecundario> secundarios;
-    private ArrayList<CondicionAtmosferica> condicionesAtmosfericas;
-    private ArrayList<TipoTerreno> tiposTerreno;
-    private ArrayList<MomentoDia> momentosDia;
+    public static ArrayList<Protagonista> protagonistas;
+    public static ArrayList<PersonajeCombatiente> enemigos;
+    public static ArrayList<PersonajeSecundario> secundarios;
 
-    // Constructor 
-    public DatosJuego() {
-        protagonistas = new ArrayList<>();
-        enemigos = new ArrayList<>();
-        secundarios = new ArrayList<>();
-        condicionesAtmosfericas = new ArrayList<>();
-        tiposTerreno = new ArrayList<>();
-        momentosDia = new ArrayList<>();
-    }
+    public static ArrayList<CondicionAtmosferica> condicionesAtmosfericas;
+    public static ArrayList<TipoTerreno> tiposTerreno;
+    public static ArrayList<MomentoDia> momentosDia;
 
-    // Método para cargar datos desde los DAOs
-    public void cargarDatos() throws SQLException {
-        protagonistas = new ArrayList<>(DaoPersonaje.getInstance().obtenerProtagonistas());
-        enemigos = new ArrayList<>(DaoPersonaje.getInstance().obtenerEnemigos());
-        secundarios = new ArrayList<>(DaoPersonaje.getInstance().obtenerSecundarios());
-        condicionesAtmosfericas = new ArrayList<>(DaoCondicionAtmosferica.getInstance().obtenerCondicionAtmosferica());
-        tiposTerreno = new ArrayList<>(DaoTipoTerreno.getInstance().obtenerTipoTerreno());
-        momentosDia = new ArrayList<>(DaoMomentoDia.getInstance().obtenerMomentoDia());
-    }
+    public  void cargarDatos() throws SQLException {
 
-    // Getters y setters
-    public ArrayList<Protagonista> getProtagonistas() {
-        return protagonistas;
-    }
-
-    public ArrayList<PersonajeCombatiente> getEnemigos() {
-        return enemigos;
-    }
-
-    public ArrayList<PersonajeSecundario> getSecundarios() {
-        return secundarios;
-    }
-
-    public ArrayList<CondicionAtmosferica> getCondicionesAtmosfericas() {
-        return condicionesAtmosfericas;
-    }
-
-    public ArrayList<TipoTerreno> getTiposTerreno() {
-        return tiposTerreno;
-    }
-
-    public ArrayList<MomentoDia> getMomentosDia() {
-        return momentosDia;
+    	protagonistas = new ArrayList<>(DaoPersonaje.getInstance().obtenerProtagonistas());
+    	enemigos = new ArrayList<>(DaoPersonaje.getInstance().obtenerEnemigos());
+    	secundarios = new ArrayList<>(DaoPersonaje.getInstance().obtenerSecundarios());
+    	condicionesAtmosfericas = new ArrayList<>(DaoCondicionAtmosferica.getInstance().obtenerCondicionAtmosferica());
+    	tiposTerreno = new ArrayList<>(DaoTipoTerreno.getInstance().obtenerTipoTerreno());
+    	momentosDia = new ArrayList<>(DaoMomentoDia.getInstance().obtenerMomentoDia());
     }
 }
