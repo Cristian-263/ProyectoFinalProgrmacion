ce9357b99fd6de8b5428f0d76c9f7c196e19a3ad
jorge1sbr <jorge.p1998@hotmail.com>
2025-06-01 18:10:34 +0200

    cambios main

--- dragonBall/src/modelo/Usuario.java
+++ dragonBall/src/modelo/Usuario.java
@@ -4,14 +4,14 @@ public class Usuario {
 
 	private String nombreUsuario;
     private String password;
-    private int puntuacion;
+ 
     
     //Constructor
     public Usuario (String nombreUsuario, String password) {
     	
     	this.nombreUsuario = nombreUsuario;
     	this.password = password;
-    	this.puntuacion = 0;
+
     }
 
 	public String getNombreUsuario() {
@@ -30,13 +30,7 @@ public class Usuario {
 		this.password = password;
 	}
 
-	public int getPuntuacion() {
-		return puntuacion;
-	}
 
-	public void setPuntuacion(int puntuacion) {
-		this.puntuacion = puntuacion;
-	}
 
-    
+
 }
